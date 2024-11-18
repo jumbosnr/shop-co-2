65b89d61f8211f270ae7d989144070c02dfeb655
Jumbo snr <tamunotonyej9@gmail.com>
2024-11-18 13:54:27 +0100

    Deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Store from './pages/store';
 import LifeGraphic from './pages/product-details';
